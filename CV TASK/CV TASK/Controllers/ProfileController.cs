d8b987778c28024c4c7e0190fb95c3b8a804c958
YourName <noor11809027@gmail.com>
2022-05-24 00:29:13 +0600

    cv task add

---
+++ CV TASK/CV TASK/Controllers/ProfileController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CV_TASK.Controllers
+{
+    public class ProfileController : Controller
+    {
+        // GET: Profile
+       // public ActionResult Index()
+        //{
+       //     return View();
+      //  }
+
+        public ActionResult Home()
+        {
+            return View();
+        }
+        public ActionResult Personal()
+        {
+            return View();
+        }
+        public ActionResult Education()
+        {
+            return View();
+        }
+
+        public ActionResult Projects()
+        {
+            return View();
+        }
+        public ActionResult Reference()
+        {
+            return View();
+        }
+    }
+}
