ab198e4fb2a9ea70a6a72ac898e341d399adcf3d
YourName <noor11809027@gmail.com>
2022-06-03 23:57:06 +0600

    lab task 1 add

---
+++ Lab Task 1/Lab Task 1/Controllers/ProductController.cs
@@ -0,0 +1,45 @@
+﻿using Lab_Task_1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab_Task_1.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            //Product p1 = new Product();
+            List<Product> products = new List<Product>();
+            for (int i = 0; i <= 10; i++)
+            {
+                products.Add(new Product()
+                {
+                    Id=i+1,
+                    Name="product"+(i+1),
+                    Description="This is a product"
+                });;
+                
+            }
+            return View(products);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Product p)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(p);
+        }
+    }
+}
---
+++ Lab Task 1/Lab Task 1/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Lab_Task_1.Models
+{
+    public class Product
+    {
+        [Required(ErrorMessage = "Please provie Id")]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please provie Name")]
+        public string Name { get; set; }
+       
+        [Required(ErrorMessage = "Please provie Description")]
+        public string Description { get; set; }
+    }
+}
